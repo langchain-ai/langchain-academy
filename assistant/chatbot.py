fc00b01453deb1a906a60f576a9b87c1a8088052
Lance Martin <lance@langchain.dev>
2024-08-19 09:43:08 -0700

    Update module 2

---
+++ assistant/chatbot.py
@@ -0,0 +1,87 @@
+from typing import Annotated, List, TypedDict
+from langchain_core.messages import AnyMessage
+from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
+from langgraph.graph.message import add_messages
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+
+# We will use this model for both the conversation and the summarization
+from langchain_anthropic import ChatAnthropic
+model = ChatAnthropic(model="claude-3-5-sonnet-20240620", temperature=0) 
+
+class State(MessagesState):
+    summary: str
+    
+# Define the logic to call the model
+def call_model(state: State):
+    
+    # Get summary if it exists
+    summary = state.get("summary", "")
+
+    # If there is summary, then we add it
+    if summary:
+        
+        # Add summary to system message
+        system_message = f"Summary of conversation earlier: {summary}"
+
+        # Append summary to any newer messages
+        messages = [SystemMessage(content=system_message)] + state["messages"]
+    
+    else:
+        messages = state["messages"]
+    
+    response = model.invoke(messages)
+    return {"messages": response}
+
+# Determine whether to end or summarize the conversation
+def should_continue(state: State):
+    
+    """Return the next node to execute."""
+    
+    messages = state["messages"]
+    
+    # If there are more than six messages, then we summarize the conversation
+    if len(messages) > 6:
+        return "summarize_conversation"
+    
+    # Otherwise we can just end
+    return END
+
+def summarize_conversation(state: State):
+    
+    # First, we summarize the conversation
+    summary = state.get("summary", "")
+
+    # Create our summarization prompt 
+    if summary:
+        
+        # If a summary already exists
+        summary_message = (
+            f"This is summary of the conversation to date: {summary}\n\n"
+            "Extend the summary by taking into account the new messages above:"
+        )
+        
+    else:
+        summary_message = "Create a summary of the conversation above:"
+
+    # Add prompt to our history
+    messages = state["messages"] + [HumanMessage(content=summary_message)]
+    response = model.invoke(messages)
+    
+    # Add our summary to the state 
+    # Delete all but the 2 most recent messages
+    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+    return {"summary": response.content, "messages": delete_messages}
+
+# Define a new graph
+workflow = StateGraph(State)
+workflow.add_node("conversation", call_model)
+workflow.add_node(summarize_conversation)
+
+# Set the entrypoint as conversation
+workflow.add_edge(START, "conversation")
+workflow.add_conditional_edges("conversation", should_continue)
+workflow.add_edge("summarize_conversation", END)
+
+# Compile
+graph = workflow.compile()
