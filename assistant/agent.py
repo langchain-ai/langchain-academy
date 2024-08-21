1db8945088216790750740e3d1a07ba8a91c0da4
Lance Martin <lance@langchain.dev>
2024-08-21 16:52:41 -0700

    Minor Module 1 updates

--- assistant/agent.py
+++ assistant/agent.py
@@ -3,7 +3,7 @@ from typing_extensions import TypedDict
 from langchain_core.messages import SystemMessage
 
 from langgraph.graph.message import AnyMessage, add_messages
-from langgraph.graph import START, StateGraph
+from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition
 from langgraph.prebuilt import ToolNode
 
@@ -39,40 +39,22 @@ def divide(a: int, b: int) -> float:
 
 tools = [add, multiply, divide]
 
+# LLM and bind tools
+llm = ChatOpenAI(model="gpt-4o")
+llm_with_tools = llm.bind_tools(tools)
+
 # System message
 sys_msg = SystemMessage(content="You are a helpful assistant tasked with writing performing arithmatic on a set of inputs.")
 
-
-# State
-class MessagesState(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages]
-
-# Assistant
-class Assistant:
-    def __init__(self, llm_with_tools):
-        """
-        Initialize the Assistant with llm.
-        """
-        self.llm_with_tools = llm_with_tools
-
-    def __call__(self, state: MessagesState):
-        """
-        Call method to invoke
-        """
-        # Get messages
-        messages = state['messages']
-        # Invoke chat model
-        result = self.llm_with_tools.invoke([sys_msg]+messages)  
-        return {"messages": result}
-
-# Prompt our LLM and bind tools
-llm = ChatOpenAI(model="gpt-4o")
+# Node
+def assistant(state: MessagesState):
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
 
 # Graph
 builder = StateGraph(MessagesState)
 
 # Define nodes: these do the work
-builder.add_node("assistant", Assistant(llm.bind_tools(tools)))
+builder.add_node("assistant", assistant)
 builder.add_node("tools", ToolNode(tools))
 
 # Define edges: these determine how the control flow moves
