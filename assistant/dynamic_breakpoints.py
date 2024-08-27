33909c23946aa60db260b373147b190104a8dd79
Lance Martin <lance@langchain.dev>
2024-08-27 14:08:43 -0700

    Update dynamic breakpoints

---
+++ assistant/dynamic_breakpoints.py
@@ -0,0 +1,38 @@
+from typing import TypedDict
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.errors import NodeInterrupt
+from langgraph.graph import START, END, StateGraph
+
+class State(TypedDict):
+    input: str
+
+def step_1(state: State) -> State:
+    print("---Step 1---")
+    return state
+
+def step_2(state: State) -> State:
+    # Let's optionally raise a NodeInterrupt if the length of the input is longer than 5 characters
+    if len(state['input']) > 5:
+        raise NodeInterrupt(f"Received input that is longer than 5 characters: {state['input']}")
+    
+    print("---Step 2---")
+    return state
+
+def step_3(state: State) -> State:
+    print("---Step 3---")
+    return state
+
+builder = StateGraph(State)
+builder.add_node("step_1", step_1)
+builder.add_node("step_2", step_2)
+builder.add_node("step_3", step_3)
+builder.add_edge(START, "step_1")
+builder.add_edge("step_1", "step_2")
+builder.add_edge("step_2", "step_3")
+builder.add_edge("step_3", END)
+
+# Set up memory
+memory = MemorySaver()
+
+# Compile the graph with memory
+graph = builder.compile(checkpointer=memory)
