15d8aa371926ff3e71ece01f4bf0b52dfc0b9354
Lance Martin <lance@langchain.dev>
2024-08-28 17:56:35 -0700

    Update

---
+++ assistant/map_reduce.py
@@ -0,0 +1,66 @@
+import operator
+from typing import Annotated, TypedDict
+
+from langchain_core.pydantic_v1 import BaseModel
+
+from langchain_anthropic import ChatAnthropic
+
+from langgraph.constants import Send
+from langgraph.graph import END, StateGraph, START
+
+# Prompts we will use
+subjects_prompt = """Generate a comma separated list of between 2 and 5 examples related to: {topic}."""
+joke_prompt = """Generate a joke about {subject}"""
+best_joke_prompt = """Below are a bunch of jokes about {topic}. Select the best one! Return the ID of the best one, starting 0 as the ID for the first joke. Jokes: \n\n  {jokes}"""
+
+# LLM
+model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+# Define the state
+class Subjects(BaseModel):
+    subjects: list[str]
+
+class BestJoke(BaseModel):
+    id: int
+    
+class OverallState(TypedDict):
+    topic: str
+    subjects: list
+    jokes: Annotated[list, operator.add]
+    best_selected_joke: str
+
+def generate_topics(state: OverallState):
+    prompt = subjects_prompt.format(topic=state["topic"])
+    response = model.with_structured_output(Subjects).invoke(prompt)
+    return {"subjects": response.subjects}
+
+class JokeState(TypedDict):
+    subject: str
+
+class Joke(BaseModel):
+    joke: str
+
+def generate_joke(state: JokeState):
+    prompt = joke_prompt.format(subject=state["subject"])
+    response = model.with_structured_output(Joke).invoke(prompt)
+    return {"jokes": [response.joke]}
+
+def best_joke(state: OverallState):
+    jokes = "\n\n".join(state["jokes"])
+    prompt = best_joke_prompt.format(topic=state["topic"], jokes=jokes)
+    response = model.with_structured_output(BestJoke).invoke(prompt)
+    return {"best_selected_joke": state["jokes"][response.id]}
+
+def continue_to_jokes(state: OverallState):
+    return [Send("generate_joke", {"subject": s}) for s in state["subjects"]]
+
+# Construct the graph: here we put everything together to construct our graph
+graph_builder = StateGraph(OverallState)
+graph_builder.add_node("generate_topics", generate_topics)
+graph_builder.add_node("generate_joke", generate_joke)
+graph_builder.add_node("best_joke", best_joke)
+graph_builder.add_edge(START, "generate_topics")
+graph_builder.add_conditional_edges("generate_topics", continue_to_jokes, ["generate_joke"])
+graph_builder.add_edge("generate_joke", "best_joke")
+graph_builder.add_edge("best_joke", END)
+graph = graph_builder.compile()
